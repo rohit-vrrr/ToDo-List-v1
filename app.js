197e7500a212e015ce9d4a07a1720d97645f1243
Rohit <48995037+rohit-vrrr@users.noreply.github.com>
2021-02-20 17:30:41 +0530

    Import module "date"
    (+) date.js
    (+) app.js

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-let items = ["Eat", "Sleep", "Repeat"];
-let workItems = [];
+const items = ["Eat", "Sleep", "Repeat"];
+const workItems = [];
 
 app.get('/', function(req, res) {
-
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
+  const day = date.getDate();
   res.render('list', {listTitle: day, newItems: items});
 });
 
 app.post('/', function(req, res) {
 
-  let item = req.body.newTask;
+  const item = req.body.newTask;
 
   if(req.body.list === "Work") {
     workItems.push(item);
