4fcabc8f69626d5938c0372996bab79275aecdfb
Rohit <48995037+rohit-vrrr@users.noreply.github.com>
2021-02-20 14:46:30 +0530

    Added Layouts in EJS
    (+) /views/header.ejs
    (+) /views/footer.ejs
    (+) /views/list.ejs

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 let items = ["Eat", "Sleep", "Repeat"];
+let workItems = [];
 
 app.get('/', function(req, res) {
 
@@ -23,9 +24,21 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
+
   let item = req.body.newTask;
-  items.push(item);
-  res.redirect("/");
+
+  if(req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
+app.get('/work', function(req, res) {
+  res.render('list', {listTitle: "Work List", newItems: workItems});
 });
 
 app.listen(3000, function() {
