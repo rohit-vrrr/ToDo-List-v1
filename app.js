a23b658fcb91c5786308168b661efb52ef1b052b
Rohit <48995037+rohit-vrrr@users.noreply.github.com>
2021-02-20 12:45:45 +0530

    Added rendering

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+var items = ["Eat", "Sleep", "Repeat"];
+
 app.get('/', function(req, res) {
 
   var today = new Date();
@@ -16,7 +18,13 @@ app.get('/', function(req, res) {
 
   var day = today.toLocaleDateString("en-US", options);
 
-  res.render('list', {kindOfDay: day});
+  res.render('list', {kindOfDay: day, newItems: items});
+});
+
+app.post('/', function(req, res) {
+  item = req.body.newTask;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
